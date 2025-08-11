f4901ae2f292e4a693cb714683c8cd5a804df07e
livandan <livandan@sjtu.edu.cn>
2025-08-11 20:58:05 +0800

    Forward declaration of all the Node-based classes.

---
+++ parser/include/classes.h
@@ -0,0 +1,15 @@
+#ifndef CLASSES_H
+#define CLASSES_H
+
+class Node;
+class Crate;
+class Item;
+class Module;
+class Function;
+class Struct;
+class Enumeration;
+class ConstantItem;
+class Trait;
+class Implementation;
+
+#endif
