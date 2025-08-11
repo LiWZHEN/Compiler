093d204fa3d817c38c4d814ccba344f4ca989627
livandan <livandan@sjtu.edu.cn>
2025-08-11 21:37:21 +0800

    Add classes in functions.h

--- parser/include/classes.h
+++ parser/include/classes.h
@@ -11,5 +11,13 @@ class Enumeration;
 class ConstantItem;
 class Trait;
 class Implementation;
+class Keyword;
+class Identifier;
+class Punctuation;
+class GenericParams;
+class FunctionParameters;
+class FunctionReturnType;
+class WhereClause;
+class BlockExpression;
 
 #endif
