c82a2a458e004112f37a1e8487f936c00450b13c
livandan <livandan@sjtu.edu.cn>
2025-08-14 10:57:39 +0800

    Add reserve keywords to the check.

--- parser/src/module.cpp
+++ parser/src/module.cpp
@@ -7,12 +7,15 @@ Keyword::Keyword(const Token &token, int &ptr) : token_(token) {
 Identifier::Identifier(const Token &token, int &ptr): token_(token) {
   const std::string &t = token.GetStr();
   if (t == "as" || t == "break" || t == "const" || t == "continue" || t == "crate"
-      || t == "else" || t == "enum" || t == "extern" || t == "false" || t == "fn"
-      || t == "for" || t == "if" || t == "impl" || t == "in" || t == "let" || t == "loop"
-      || t == "match" || t == "mod" || t == "move" || t == "mut" || t == "pub"
+      || t == "dyn" || t == "else" || t == "enum" || t == "false" || t == "fn"
+      || t == "for" || t == "if" || t == "impl" || t == "in" || t == "let"
+      || t == "loop" || t == "match" || t == "mod" || t == "move" || t == "mut"
       || t == "ref" || t == "return" || t == "self" || t == "Self" || t == "static"
       || t == "struct" || t == "super" || t == "trait" || t == "true" || t == "type"
-      || t == "unsafe" || t == "use" || t == "where" || t == "while") {
+      || t == "unsafe" || t == "use" || t == "where" || t == "while" || t == "abstract"
+      || t == "become" || t == "box" || t == "do" || t == "final" || t == "macro"
+      || t == "override" || t == "priv" || t == "typeof" || t == "unsized"
+      || t == "virtual" || t == "yield" || t == "try" || t == "gen") {
     std::cerr << "Cannot use keyword as identifier.\n";
     throw "";
   }
