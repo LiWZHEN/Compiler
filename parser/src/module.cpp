5eb6c37024c015f80be1d6e8dc17b81c3626fc5f
livandan <livandan@sjtu.edu.cn>
2025-08-14 10:48:11 +0800

    Check to avoid using keywords as identifiers.

--- parser/src/module.cpp
+++ parser/src/module.cpp
@@ -5,6 +5,17 @@ Keyword::Keyword(const Token &token, int &ptr) : token_(token) {
 }
 
 Identifier::Identifier(const Token &token, int &ptr): token_(token) {
+  const std::string &t = token.GetStr();
+  if (t == "as" || t == "break" || t == "const" || t == "continue" || t == "crate"
+      || t == "else" || t == "enum" || t == "extern" || t == "false" || t == "fn"
+      || t == "for" || t == "if" || t == "impl" || t == "in" || t == "let" || t == "loop"
+      || t == "match" || t == "mod" || t == "move" || t == "mut" || t == "pub"
+      || t == "ref" || t == "return" || t == "self" || t == "Self" || t == "static"
+      || t == "struct" || t == "super" || t == "trait" || t == "true" || t == "type"
+      || t == "unsafe" || t == "use" || t == "where" || t == "while") {
+    std::cerr << "Cannot use keyword as identifier.\n";
+    throw "";
+  }
   ++ptr;
 }
 
