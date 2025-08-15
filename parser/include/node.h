3cada87b5d52acb784807ff90368107b6051f09c
livandan <livandan@sjtu.edu.cn>
2025-08-15 17:35:27 +0800

    Add some types and functions.

--- parser/include/node.h
+++ parser/include/node.h
@@ -6,18 +6,23 @@
 #include "token.h"
 
 enum NodeType {
-  type_crate, type_item, type_module, type_function, type_struct, type_enumeration,
+  type_crate, type_item, type_function, type_struct, type_enumeration,
   type_constant_item, type_trait, type_implementation, type_keyword, type_identifier,
   type_punctuation, type_generic_params, type_function_parameters, type_function_return_type,
   type_where_clause, type_block_expression, type_struct_fields, type_enum_variants,
   type_type, type_expression, type_type_param_bounds, type_associated_item,
-  type_type_path, type_generic_param
+  type_type_path, type_generic_param, type_self_param, type_shorthand_self, type_typed_self,
+  type_function_param, type_function_param_pattern, type_where_clause_item,
+  type_statements
 };
 
 class Node {
 public:
   ~Node();
 protected:
+  void AddChild(NodeType node_type, const std::vector<Token> &tokens, int &ptr);
+  void ThrowErr(NodeType node_type, const std::string &info, bool is_ending, int line, int column);
+  void Restore(int size_before_try);
   std::vector<Node *> children_;
   std::vector<NodeType> type_;
 };
