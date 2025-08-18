da41979e08ee0abe5d307a6e524bc71f143ed901
livandan <livandan@sjtu.edu.cn>
2025-08-18 09:16:19 +0800

    Throw when cannot match.

--- parser/src/expression.cpp
+++ parser/src/expression.cpp
@@ -1,10 +1,10 @@
 #include "expression.h"
 
-LiteralExpression::LiteralExpression(const std::vector<Token> &tokens, int &ptr) : Node(tokens, ptr), token_(tokens[ptr]) {
+LiteralExpression::LiteralExpression(const std::vector<Token> &tokens, int &ptr) : LeafNode(tokens, ptr) {
   const std::string next_token = tokens[ptr].GetStr();
   const type next_type = tokens[ptr].GetType();
-  if (next_type == CHAR_LITERAL || next_type == STRING_LITERAL || next_type == RAW_STRING_LITERAL || next_type == C_STRING_LITERAL
-      || next_type == RAW_C_STRING_LITERAL || next_type == INTEGER_LITERAL || next_token == "true" || next_token == "false") {
-
+  if (next_type != CHAR_LITERAL && next_type != STRING_LITERAL && next_type != RAW_STRING_LITERAL && next_type != C_STRING_LITERAL
+      && next_type != RAW_C_STRING_LITERAL && next_type != INTEGER_LITERAL && next_token != "true" && next_token != "false") {
+    ThrowErr(type_literal_expression, "Invalid literal expression.");
   }
 }
