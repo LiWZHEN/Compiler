2016269ba3ac6a8721137acaabe4d5643cf872d1
livandan <livandan@sjtu.edu.cn>
2025-08-18 17:11:44 +0800

    Add functions to get info.'

--- parser/include/node.h
+++ parser/include/node.h
@@ -9,17 +9,22 @@ enum NodeType {
   type_crate, type_item, type_function, type_struct, type_enumeration, type_constant_item,
   type_trait, type_implementation, type_keyword, type_identifier, type_punctuation,
   type_function_parameters, type_function_return_type, type_block_expression, type_struct_fields,
-  type_enum_variants, type_type, type_expression, type_type_param_bounds, type_associated_item,
-  type_type_path, type_self_param, type_shorthand_self, type_typed_self, type_function_param,
-  type_function_param_pattern, type_statements, type_pattern, type_literal_pattern,
-  type_identifier_pattern, type_wildcard_pattern, type_reference_pattern, type_tuple_struct_pattern,
-  type_path_pattern, type_literal_expression, type_path_in_expression, type_tuple_struct_items
+  type_enum_variants, type_type, type_type_param_bounds, type_associated_item, type_type_path,
+  type_self_param, type_shorthand_self, type_typed_self, type_function_param, type_statements,
+  type_pattern, type_literal_pattern, type_identifier_pattern, type_wildcard_pattern,
+  type_reference_pattern, type_tuple_struct_pattern, type_literal_expression, type_path_in_expression,
+  type_tuple_struct_items, type_path_expr_segment, type_reference_type, type_array_type,
+  type_slice_type, type_inferred_type, type_expression, type_expression_without_block,
+  type_expression_with_block
 };
 
 class Node {
 public:
   Node(const std::vector<Token> &tokens, int &ptr);
   ~Node();
+  [[nodiscard]] int GetChildrenNum() const;
+  [[nodiscard]] std::vector<Node *> const &GetChildrenPtr() const;
+  [[nodiscard]] std::vector<NodeType> const &GetChildrenType() const;
 protected:
   void AddChild(NodeType node_type);
   void ThrowErr(NodeType node_type, const std::string &info);
@@ -33,8 +38,8 @@ protected:
 class LeafNode : public Node {
 public:
   LeafNode(const std::vector<Token> &tokens, int &ptr);
-protected:
   [[nodiscard]] Token const &GetContent() const;
+protected:
   Token token_;
 };
 
