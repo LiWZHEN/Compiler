d0a9813e0cbac08d9622bc48fa901a535f18f5c5
livandan <livandan@sjtu.edu.cn>
2025-08-22 11:07:57 +0800

    Remove some currently not used types.

--- parser/include/node.h
+++ parser/include/node.h
@@ -4,7 +4,6 @@
 #include <vector>
 #include "classes.h"
 #include "token.h"
-#include "expression.h"
 
 enum NodeType {
   type_crate, type_item, type_function, type_struct, type_enumeration, type_constant_item,
@@ -15,16 +14,20 @@ enum NodeType {
   type_pattern, type_literal_pattern, type_identifier_pattern, type_wildcard_pattern,
   type_reference_pattern, type_tuple_struct_pattern, type_literal_expression, type_path_in_expression,
   type_tuple_struct_items, type_path_expr_segment, type_reference_type, type_array_type,
-  type_slice_type, type_inferred_type, type_expression
+  type_slice_type, type_inferred_type, type_expression, type_char_literal, type_string_literal,
+  type_raw_string_literal, type_c_string_literal, type_raw_c_string_literal, type_integer_literal,
+  type_struct_expr_field, type_struct_expr_fields
 };
 
 class Node {
 public:
   Node(const std::vector<Token> &tokens, int &ptr);
-  ~Node();
+  virtual ~Node();
   [[nodiscard]] int GetChildrenNum() const;
   [[nodiscard]] std::vector<Node *> const &GetChildrenPtr() const;
   [[nodiscard]] std::vector<NodeType> const &GetChildrenType() const;
+  [[nodiscard]] virtual std::string GetStruct(const std::string& prefix, bool isLast) const;
+  [[nodiscard]] virtual std::string GetNodeLabel() const;
 protected:
   void AddChild(NodeType node_type);
   // void AddExpr();
@@ -40,6 +43,7 @@ class LeafNode : public Node {
 public:
   LeafNode(const std::vector<Token> &tokens, int &ptr);
   [[nodiscard]] Token const &GetContent() const;
+  [[nodiscard]] std::string GetNodeLabel() const override;
 protected:
   Token token_;
 };
--- parser/src/node.cpp
+++ parser/src/node.cpp
@@ -2,12 +2,9 @@
 #include "item.h"
 #include "leaf_node.h"
 #include "functions.h"
-#include "structs.h"
-#include "enumerations.h"
-#include "trait.h"
-#include "block_expression.h"
-#include "function_parameters.h"
-#include "type.h"
+#include "path.h"
+#include "expression.h"
+#include "statements.h"
 
 Node::Node(const std::vector<Token> &tokens, int &ptr) : tokens_(tokens), ptr_(ptr) {}
 
@@ -35,142 +32,87 @@ std::vector<NodeType> const &Node::GetChildrenType() const {
 void Node::AddChild(NodeType node_type) {
   const int target = static_cast<int>(children_.size());
   switch (node_type) {
-    case type_item :
+    case type_item:
       children_.push_back(nullptr);
       children_[target] = new Item(tokens_, ptr_);
       type_.push_back(type_item);
       break;
-    case type_function :
+    case type_function:
       children_.push_back(nullptr);
       children_[target] = new Function(tokens_, ptr_);
       type_.push_back(type_function);
       break;
-    case type_struct :
+    case type_struct:
       children_.push_back(nullptr);
       children_[target] = new Struct(tokens_, ptr_);
       type_.push_back(type_struct);
       break;
-    case type_enumeration :
+    case type_enumeration:
       children_.push_back(nullptr);
       children_[target] = new Enumeration(tokens_, ptr_);
       type_.push_back(type_enumeration);
       break;
-    case type_constant_item :
+    case type_constant_item:
       children_.push_back(nullptr);
       children_[target] = new ConstantItem(tokens_, ptr_);
       type_.push_back(type_constant_item);
       break;
-    case type_trait :
+    case type_trait:
       children_.push_back(nullptr);
       children_[target] = new Trait(tokens_, ptr_);
       type_.push_back(type_trait);
       break;
-    case type_implementation :
+    case type_implementation:
       children_.push_back(nullptr);
       children_[target] = new Implementation(tokens_, ptr_);
       type_.push_back(type_implementation);
       break;
-    case type_keyword :
+    case type_keyword:
       children_.push_back(nullptr);
       children_[target] = new Keyword(tokens_, ptr_);
       type_.push_back(type_keyword);
       break;
-    case type_identifier :
+    case type_identifier:
       children_.push_back(nullptr);
       children_[target] = new Identifier(tokens_, ptr_);
       type_.push_back(type_identifier);
       break;
-    case type_punctuation :
+    case type_punctuation:
       children_.push_back(nullptr);
       children_[target] = new Punctuation(tokens_, ptr_);
       type_.push_back(type_punctuation);
       break;
-    /*case type_generic_params :
-      children_.push_back(nullptr);
-      children_[target] = new GenericParams(tokens_, ptr_);
-      type_.push_back(type_generic_params);
-      break;*/
-    case type_function_parameters :
+    case type_function_parameters:
       children_.push_back(nullptr);
       children_[target] = new FunctionParameters(tokens_, ptr_);
       type_.push_back(type_function_parameters);
       break;
-    case type_function_return_type :
+    case type_function_return_type:
       children_.push_back(nullptr);
       children_[target] = new FunctionReturnType(tokens_, ptr_);
       type_.push_back(type_function_return_type);
       break;
-    /*case type_where_clause :
-      children_.push_back(nullptr);
-      children_[target] = new WhereClause(tokens_, ptr_);
-      type_.push_back(type_where_clause);
-      break;*/
-    case type_block_expression :
+    case type_block_expression:
       children_.push_back(nullptr);
       children_[target] = new BlockExpression(tokens_, ptr_);
       type_.push_back(type_block_expression);
       break;
-    case type_struct_fields :
+    case type_path_in_expression:
       children_.push_back(nullptr);
-      children_[target] = new StructFields(tokens_, ptr_);
-      type_.push_back(type_struct_fields);
+      children_[target] = new PathInExpression(tokens_, ptr_);
+      type_.push_back(type_path_in_expression);
       break;
-    case type_enum_variants :
+    case type_path_expr_segment:
       children_.push_back(nullptr);
-      children_[target] = new EnumVariants(tokens_, ptr_);
-      type_.push_back(type_enum_variants);
+      children_[target] = new PathExprSegment(tokens_, ptr_);
+      type_.push_back(type_path_expr_segment);
       break;
-    case type_type :
+    case type_integer_literal:
       children_.push_back(nullptr);
-      children_[target] = new Type(tokens_, ptr_);
-      type_.push_back(type_type);
+      children_[target] = new IntegerLiteral(tokens_, ptr_);
+      type_.push_back(type_integer_literal);
       break;
-    case type_type_param_bounds :
-      children_.push_back(nullptr);
-      children_[target] = new TypeParamBounds(tokens_, ptr_);
-      type_.push_back(type_type_param_bounds);
-      break;
-    case type_associated_item :
-      children_.push_back(nullptr);
-      children_[target] = new AssociatedItem(tokens_, ptr_);
-      type_.push_back(type_associated_item);
-      break;
-    case type_type_path :
-      children_.push_back(nullptr);
-      children_[target] = new TypePath(tokens_, ptr_);
-      type_.push_back(type_type_path);
-      break;
-    /*case type_generic_param :
-      children_.push_back(nullptr);
-      children_[target] = new GenericParam(tokens_, ptr_);
-      type_.push_back(type_generic_param);
-      break;*/
-    case type_self_param :
-      children_.push_back(nullptr);
-      children_[target] = new SelfParam(tokens_, ptr_);
-      type_.push_back(type_self_param);
-      break;
-    case type_shorthand_self :
-      children_.push_back(nullptr);
-      children_[target] = new ShorthandSelf(tokens_, ptr_);
-      type_.push_back(type_shorthand_self);
-      break;
-    case type_typed_self :
-      children_.push_back(nullptr);
-      children_[target] = new TypedSelf(tokens_, ptr_);
-      type_.push_back(type_typed_self);
-      break;
-    case type_function_param :
-      children_.push_back(nullptr);
-      children_[target] = new FunctionParam(tokens_, ptr_);
-      type_.push_back(type_function_param);
-      break;
-    /*case type_where_clause_item :
-      children_.push_back(nullptr);
-      children_[target] = new WhereClauseItem(tokens_, ptr_);
-      type_.push_back(type_where_clause_item);
-      break;*/
-    case type_statements :
+    case type_statements:
       children_.push_back(nullptr);
       children_[target] = new Statements(tokens_, ptr_);
       type_.push_back(type_statements);
@@ -180,40 +122,6 @@ void Node::AddChild(NodeType node_type) {
       throw "";
   }
 }
-
-/*void Node::AddExpr() {
-  const std::string next_token = tokens_[ptr_].GetStr();
-  const int target = static_cast<int>(children_.size());
-  children_.push_back(nullptr);
-  if (next_token == "{") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, block_expr, 0));
-  } else if (next_token == "const") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, const_block_expr, 0));
-  } else if (next_token == "loop") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, infinite_loop_expr, 0));
-  } else if (next_token == "while") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, predicate_loop_expr, 0));
-  } else if (next_token == "if") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, if_expr, 0));
-  } else if (next_token == "match") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, match_expr, 0));
-  } else if (next_token == "(") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, grouped_expr, 0));
-  } else if (next_token == "[") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, array_expr, 0));
-  } else if (next_token == "continue") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, continue_expr, 0));
-  } else if (next_token == "break") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, break_expr, 0));
-  } else if (next_token == "return") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, return_expr, 0));
-  } else if (next_token == "_") {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, underscore_expr, 0));
-  } else {
-    children_[target] = reinterpret_cast<Node *>(new Expression(tokens_, ptr_, unknown, 0));
-  }
-  type_.push_back(type_expression);
-}*/
 
 void Node::ThrowErr(const NodeType node_type, const std::string &info) {
   switch (node_type) {
@@ -250,18 +158,12 @@ void Node::ThrowErr(const NodeType node_type, const std::string &info) {
     case type_punctuation:
       std::cerr << "Punctuation: ";
       break;
-    /*case type_generic_params:
-      std::cerr << "GenericParams: ";
-      break;*/
     case type_function_parameters:
       std::cerr << "FunctionParameters: ";
       break;
     case type_function_return_type:
       std::cerr << "ReturnType: ";
       break;
-    /*case type_where_clause:
-      std::cerr << "WhereClause: ";
-      break;*/
     case type_block_expression:
       std::cerr << "BlockExpression: ";
       break;
@@ -283,9 +185,6 @@ void Node::ThrowErr(const NodeType node_type, const std::string &info) {
     case type_type_path:
       std::cerr << "TypePath: ";
       break;
-    /*case type_generic_param:
-      std::cerr << "GenericParam: ";
-      break;*/
     case type_self_param:
       std::cerr << "SelfParam: ";
       break;
@@ -298,11 +197,11 @@ void Node::ThrowErr(const NodeType node_type, const std::string &info) {
     case type_function_param:
       std::cerr << "FunctionParam: ";
       break;
-    /*case type_where_clause_item:
-      std::cerr << "WhereClauseItem: ";
-      break;*/
     case type_statements:
       std::cerr << "Statements: ";
+      break;
+    case type_path_expr_segment:
+      std::cerr << "PathExprSegment: ";
       break;
     default:
       std::cerr << "No matched type!\n";
@@ -373,7 +272,29 @@ Item::Item(const std::vector<Token> &tokens, int &ptr) : Node(tokens, ptr) {
       ThrowErr(type_item, "Invalid keyword!");
     }
   } catch (...) {
-    Restore(0, ptr);
+    Restore(0, ptr_before_try);
     throw "";
   }
+}
+
+
+// The following functions for printing is provided by deepseek
+std::string Node::GetNodeLabel() const {
+  return "Node";
+}
+
+std::string LeafNode::GetNodeLabel() const {
+  return "Leaf: " + token_.GetStr();
+}
+
+std::string Node::GetStruct(const std::string& prefix, bool isLast) const {
+  std::string result;
+  result += prefix + (isLast ? "└── " : "├── ") + GetNodeLabel() + "\n"; // Show current node
+  std::string childPrefix = prefix + (isLast ? "    " : "│   ");
+  // Recursive processing
+  for (size_t i = 0; i < children_.size(); ++i) {
+    bool childIsLast = (i == children_.size() - 1);
+    result += children_[i]->GetStruct(childPrefix, childIsLast);
+  }
+  return result;
 }
