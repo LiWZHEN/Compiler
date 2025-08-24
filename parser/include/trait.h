09d54ca09686275bf6c77f27d0a36f2f2de0856c
livandan <livandan@sjtu.edu.cn>
2025-08-24 14:46:04 +0800

    Remove TypeParamBounds.

--- parser/include/trait.h
+++ parser/include/trait.h
@@ -4,11 +4,6 @@
 #include "classes.h"
 #include "node.h"
 
-class TypeParamBounds : public Node {
-public:
-  TypeParamBounds(const std::vector<Token> &tokens, int &ptr);
-};
-
 class AssociatedItem : public Node {
 public:
   AssociatedItem(const std::vector<Token> &tokens, int &ptr);
