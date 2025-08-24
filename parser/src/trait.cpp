9fb7a171d58ea378036c91d11d98b4ad589ab591
livandan <livandan@sjtu.edu.cn>
2025-08-24 14:23:53 +0800

    Complete AssociatedItem.

---
+++ parser/src/trait.cpp
@@ -0,0 +1,25 @@
+#include "trait.h"
+
+AssociatedItem::AssociatedItem(const std::vector<Token> &tokens, int &ptr) : Node(tokens, ptr) {
+  const int ptr_before_try = ptr_;
+  try {
+    const std::string next_token = tokens_[ptr_].GetStr();
+    if (next_token == "fn") {
+      AddChild(type_function);
+    } else if (next_token == "const") {
+      if (ptr_ + 1 >= tokens_.size()) {
+        ThrowErr(type_associated_item, "Expect ConstantItem or Function.");
+      }
+      if (tokens_[ptr_ + 1].GetStr() == "fn") {
+        AddChild(type_function);
+      } else {
+        AddChild(type_constant_item);
+      }
+    } else {
+      ThrowErr(type_associated_item, "Expect ConstantItem or Function.");
+    }
+  } catch (...) {
+    Restore(0, ptr_before_try);
+    throw "";
+  }
+}
