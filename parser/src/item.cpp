3a612e2e1e1ed0a884fd464b432670ae1b308be0
livandan <livandan@sjtu.edu.cn>
2025-08-24 14:44:20 +0800

    Polish item-related functions.

--- parser/src/item.cpp
+++ parser/src/item.cpp
@@ -4,62 +4,62 @@ Function::Function(const std::vector<Token> &tokens, int &ptr) : Node(tokens, pt
   const int ptr_before_try = ptr_;
   try {
     // const?
-    if (tokens[ptr].GetStr() == "const") {
+    if (tokens_[ptr_].GetStr() == "const") {
       AddChild(type_keyword);
     }
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_function, "");
     }
     // fn
-    if (tokens[ptr].GetStr() != "fn") {
+    if (tokens_[ptr_].GetStr() != "fn") {
       ThrowErr(type_function, "Expect \"fn\".");
     }
     AddChild(type_keyword);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_function, "");
     }
-    // IDENTIFIER
-    if (tokens[ptr].GetType() != IDENTIFIER_OR_KEYWORD) {
-      ThrowErr(type_function, "Expect IDENTIFIER.");
+    // Identifier
+    if (tokens_[ptr_].GetType() != IDENTIFIER_OR_KEYWORD) {
+      ThrowErr(type_function, "Expect Identifier.");
     }
     AddChild(type_identifier);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_function, "");
     }
     // (
-    if (tokens[ptr].GetStr() != "(") {
+    if (tokens_[ptr_].GetStr() != "(") {
       ThrowErr(type_function, "Expect \"(\".");
     }
     AddChild(type_punctuation);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_function, "");
     }
     // FunctionParameters?
-    if (tokens[ptr].GetStr() != ")") {
+    if (tokens_[ptr_].GetStr() != ")") {
       AddChild(type_function_parameters);
-      if (ptr >= tokens.size()) {
+      if (ptr_ >= tokens_.size()) {
         ThrowErr(type_function, "");
       }
     }
     // )
-    if (tokens[ptr].GetStr() != ")") {
+    if (tokens_[ptr_].GetStr() != ")") {
       ThrowErr(type_function, "Expect \")\".");
     }
     AddChild(type_punctuation);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_function, "");
     }
     // FunctionReturnType?
-    if (tokens[ptr].GetStr() == "->") {
+    if (tokens_[ptr_].GetStr() == "->") {
       AddChild(type_function_return_type);
-      if (ptr >= tokens.size()) {
+      if (ptr_ >= tokens_.size()) {
         ThrowErr(type_function, "");
       }
     }
     // ;|BlockExpression
-    if (tokens[ptr].GetStr() == ";") {
+    if (tokens_[ptr_].GetStr() == ";") {
       AddChild(type_punctuation);
-    } else if (tokens[ptr].GetStr() == "{") {
+    } else if (tokens_[ptr_].GetStr() == "{") {
       AddChild(type_block_expression);
     } else {
       ThrowErr(type_function, "Expect \";\" or BlockExpression.");
@@ -74,40 +74,40 @@ Struct::Struct(const std::vector<Token> &tokens, int &ptr) : Node(tokens, ptr) {
   const int ptr_before_try = ptr_;
   try {
     // struct
-    if (tokens[ptr].GetStr() != "struct") {
+    if (tokens_[ptr_].GetStr() != "struct") {
       ThrowErr(type_struct, "Expect \"struct\".");
     }
     AddChild(type_keyword);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_struct, "");
     }
-    // IDENTIFIER
-    if (tokens[ptr].GetType() != IDENTIFIER_OR_KEYWORD) {
-      ThrowErr(type_struct, "Expect IDENTIFIER.");
+    // Identifier
+    if (tokens_[ptr_].GetType() != IDENTIFIER_OR_KEYWORD) {
+      ThrowErr(type_struct, "Expect Identifier.");
     }
     AddChild(type_identifier);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_struct, "");
     }
-    if (tokens[ptr].GetStr() == ";") {
+    if (tokens_[ptr_].GetStr() == ";") {
       // ;
       AddChild(type_punctuation);
-    } else if (tokens[ptr].GetStr() == "{") {
+    } else if (tokens_[ptr_].GetStr() == "{") {
       // {StructFields?}
       // {
       AddChild(type_punctuation);
-      if (ptr >= tokens.size()) {
+      if (ptr_ >= tokens_.size()) {
         ThrowErr(type_struct, "");
       }
-      if (tokens[ptr].GetStr() != "}") {
+      if (tokens_[ptr_].GetStr() != "}") {
         // StructFields
         AddChild(type_struct_fields);
-        if (ptr >= tokens.size()) {
+        if (ptr_ >= tokens_.size()) {
           ThrowErr(type_struct, "");
         }
       }
       // }
-      if (tokens[ptr].GetStr() != "}") {
+      if (tokens_[ptr_].GetStr() != "}") {
         ThrowErr(type_struct, "Expect \"}\".");
       }
       AddChild(type_punctuation);
@@ -124,39 +124,39 @@ Enumeration::Enumeration(const std::vector<Token> &tokens, int &ptr) : Node(toke
   const int ptr_before_try = ptr_;
   try {
     // enum
-    if (tokens[ptr].GetStr() != "enum") {
+    if (tokens_[ptr_].GetStr() != "enum") {
       ThrowErr(type_enumeration, "Expect \"enum\".");
     }
     AddChild(type_keyword);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_enumeration, "");
     }
-    // IDENTIFIER
-    if (tokens[ptr].GetType() != IDENTIFIER_OR_KEYWORD) {
-      ThrowErr(type_enumeration, "Expect IDENTIFIER.");
+    // Identifier
+    if (tokens_[ptr_].GetType() != IDENTIFIER_OR_KEYWORD) {
+      ThrowErr(type_enumeration, "Expect Identifier.");
     }
     AddChild(type_identifier);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_enumeration, "");
     }
     // {
-    if (tokens[ptr].GetStr() != "{") {
+    if (tokens_[ptr_].GetStr() != "{") {
       ThrowErr(type_enumeration, "Expect \"{\".");
     }
     AddChild(type_punctuation);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_enumeration, "");
     }
     // EnumVariants?
-    if (tokens[ptr].GetStr() != "}") {
+    if (tokens_[ptr_].GetStr() != "}") {
       // EnumVariants
       AddChild(type_enum_variants);
-      if (ptr >= tokens.size()) {
+      if (ptr_ >= tokens_.size()) {
         ThrowErr(type_enumeration, "");
       }
     }
     // }
-    if (tokens[ptr].GetStr() != "}") {
+    if (tokens_[ptr_].GetStr() != "}") {
       ThrowErr(type_enumeration, "Expect \"}\".");
     }
     AddChild(type_punctuation);
@@ -170,50 +170,50 @@ ConstantItem::ConstantItem(const std::vector<Token> &tokens, int &ptr) : Node(to
   const int ptr_before_try = ptr_;
   try {
     // const
-    if (tokens[ptr].GetStr() != "const") {
+    if (tokens_[ptr_].GetStr() != "const") {
       ThrowErr(type_constant_item, "Expect \"const\".");
     }
     AddChild(type_keyword);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_constant_item, "");
     }
-    // IDENTIFIER
-    if (tokens[ptr].GetType() == IDENTIFIER_OR_KEYWORD) {
+    // Identifier
+    if (tokens_[ptr_].GetType() == IDENTIFIER_OR_KEYWORD) {
       AddChild(type_identifier);
     } else {
-      ThrowErr(type_constant_item, "Expect IDENTIFIER.");
+      ThrowErr(type_constant_item, "Expect Identifier.");
     }
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_constant_item, "");
     }
     // :
-    if (tokens[ptr].GetStr() != ":") {
+    if (tokens_[ptr_].GetStr() != ":") {
       ThrowErr(type_constant_item, "Expect \":\".");
     }
     AddChild(type_punctuation);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_constant_item, "");
     }
     // Type
     AddChild(type_type);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_constant_item, "");
     }
-    // (=Expression)?
-    if (tokens[ptr].GetStr() == "=") {
+    // (= Expression)?
+    if (tokens_[ptr_].GetStr() == "=") {
       // =
       AddChild(type_punctuation);
-      if (ptr >= tokens.size()) {
+      if (ptr_ >= tokens_.size()) {
         ThrowErr(type_constant_item, "");
       }
       // Expression
       AddChild(type_expression);
-      if (ptr >= tokens.size()) {
+      if (ptr_ >= tokens_.size()) {
         ThrowErr(type_constant_item, "");
       }
     }
     // ;
-    if (tokens[ptr].GetStr() != ";") {
+    if (tokens_[ptr_].GetStr() != ";") {
       ThrowErr(type_constant_item, "Expect \";\".");
     }
     AddChild(type_punctuation);
@@ -227,34 +227,34 @@ Trait::Trait(const std::vector<Token> &tokens, int &ptr) : Node(tokens, ptr) {
   const int ptr_before_try = ptr_;
   try {
     // trait
-    if (tokens[ptr].GetStr() != "trait") {
+    if (tokens_[ptr_].GetStr() != "trait") {
       ThrowErr(type_trait, "Expect \"trait\".");
     }
     AddChild(type_keyword);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_trait, "");
     }
-    // IDENTIFIER
-    if (tokens[ptr].GetType() != IDENTIFIER_OR_KEYWORD) {
-      ThrowErr(type_trait, "Expect IDENTIFIER.");
+    // Identifier
+    if (tokens_[ptr_].GetType() != IDENTIFIER_OR_KEYWORD) {
+      ThrowErr(type_trait, "Expect Identifier.");
     }
     AddChild(type_identifier);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_trait, "");
     }
     // {
-    if (tokens[ptr].GetStr() != "{") {
+    if (tokens_[ptr_].GetStr() != "{") {
       ThrowErr(type_trait, "Expect \"{\".");
     }
     AddChild(type_punctuation);
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_trait, "");
     }
     // AssociatedItem*
-    while (tokens[ptr].GetStr() != "}") {
+    while (tokens_[ptr_].GetStr() != "}") {
       // AssociatedItem
       AddChild(type_associated_item);
-      if (ptr >= tokens.size()) {
+      if (ptr_ >= tokens_.size()) {
         ThrowErr(type_trait, "");
       }
     }
@@ -270,35 +270,43 @@ Implementation::Implementation(const std::vector<Token> &tokens, int &ptr) : Nod
   const int ptr_before_try = ptr_;
   try {
     // impl
-    if (tokens[ptr].GetStr() != "impl") {
+    if (tokens_[ptr_].GetStr() != "impl") {
       ThrowErr(type_implementation, "Expect \"impl\".");
     }
     AddChild(type_keyword);
-    if (ptr >= tokens.size()) {
-      ThrowErr(type_implementation, "");
-    }
-    if (ptr + 1 >= tokens.size()) {
+    // (Identifier for)?
+    if (ptr_ + 1 >= tokens_.size()) {
       ThrowErr(type_implementation, "Incomplete implementation type.");
     }
-    if (tokens[ptr + 1].GetStr() == "for") {
-      // TraitImpl
+    if (tokens_[ptr_ + 1].GetStr() == "for") {
+      // Identifier
+      if (tokens_[ptr_].GetType() != IDENTIFIER_OR_KEYWORD) {
+        ThrowErr(type_implementation, "Expect Identifier.");
+      }
       AddChild(type_identifier);
+      // for
       AddChild(type_keyword);
-    }
-    if (ptr >= tokens.size()) {
-      ThrowErr(type_implementation, "");
+      if (ptr_ >= tokens_.size()) {
+        ThrowErr(type_implementation, "");
+      }
     }
     AddChild(type_type);
-    if (ptr >= tokens.size()) {
+    // {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_implementation, "");
+    }
+    if (tokens_[ptr_].GetStr() != "{") {
+      ThrowErr(type_implementation, "Expect \"{\".");
     }
     AddChild(type_punctuation);
-    while (ptr < tokens.size() && tokens[ptr].GetStr() != "}") {
+    // AssociatedItem*
+    while (ptr_ < tokens_.size() && tokens_[ptr_].GetStr() != "}") {
       AddChild(type_associated_item);
     }
-    if (ptr >= tokens.size()) {
+    if (ptr_ >= tokens_.size()) {
       ThrowErr(type_implementation, "");
     }
+    // }
     AddChild(type_punctuation);
   } catch (...) {
     Restore(0, ptr_before_try);
