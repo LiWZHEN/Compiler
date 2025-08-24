c82819d221c904611f6a23dae7a4094bb553ab2f
livandan <livandan@sjtu.edu.cn>
2025-08-24 14:06:16 +0800

    Complete EnumVariants.

---
+++ parser/src/enumerations.cpp
@@ -0,0 +1,31 @@
+#include "enumerations.h"
+
+EnumVariants::EnumVariants(const std::vector<Token> &tokens, int &ptr) : Node(tokens, ptr) {
+  const int ptr_before_try = ptr_;
+  try {
+    // Identifier
+    if (tokens_[ptr_].GetType() != IDENTIFIER_OR_KEYWORD) {
+      ThrowErr(type_enum_variants, "Expect EnumVariant.");
+    }
+    AddChild(type_identifier);
+    while (ptr_ < tokens_.size() && tokens_[ptr_].GetStr() == ",") {
+      // ,
+      AddChild(type_punctuation);
+      if (ptr_ >= tokens_.size() || tokens_[ptr_].GetType() != IDENTIFIER_OR_KEYWORD) {
+        break;
+      }
+      const int size_before_trying_identifier = static_cast<int>(children_.size()),
+          ptr_before_trying_identifier = ptr_;
+      try {
+        // Identifier
+        AddChild(type_identifier);
+      } catch (...) {
+        Restore(size_before_trying_identifier, ptr_before_trying_identifier);
+        std::cerr << "EnumVariants: Successfully handle identifier try failure.\n";
+        break;
+      }
+    }
+  } catch (...) {
+    Restore(0, ptr_before_try);
+  }
+}
